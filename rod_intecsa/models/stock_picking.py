c9ba1433d965e144600fa890d15ae53a832a99b4
Rodrigo <kurt.gf@gmail.com>
2024-03-27 11:42:43 -0400

    [FIX]arreglos stock picking

--- rod_intecsa/models/stock_picking.py
+++ rod_intecsa/models/stock_picking.py
@@ -6,10 +6,13 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         res = super(StockPicking, self).button_validate()
-        for rec in self.sale_id.order_line:
-            sum_total_cost = sum(self.move_line_ids_without_package.filtered(lambda x: x.product_id.id == rec.product_id.id).mapped('cost_product'))
-            quant = len(self.move_line_ids_without_package.filtered(lambda x: x.product_id.id == rec.product_id.id))
-            rec.purchase_price = sum_total_cost/quant
+        try:
+            for rec in self.sale_id.order_line:
+                sum_total_cost = sum(self.move_line_ids_without_package.filtered(lambda x: x.product_id.id == rec.product_id.id).mapped('cost_product'))
+                quant = len(self.move_line_ids_without_package.filtered(lambda x: x.product_id.id == rec.product_id.id))
+                rec.purchase_price = sum_total_cost/quant
+        except Exception as e:
+            print(e)
         return res
 
 
